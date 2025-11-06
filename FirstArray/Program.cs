eef7ffb20a29c2b359e860030e6533aef6940817
Cam2559 <cameronwalters10@gmail.com>
2025-11-05 22:54:04 -0500

    in progress array

---
+++ FirstArray/Program.cs
@@ -0,0 +1,19 @@
+﻿string apartments = "A105, B308, C555, B432, B222, A006";
+string[] splitResult = apartments.Split(',');
+Array.Sort(splitResult);
+
+Console.WriteLine("HalfBlood Aparment Complex");
+
+foreach (var aptNums in apartments)
+{
+    if (apartments == "C55") 
+    {
+        Console.WriteLine(apartments + "\t\tMaintenance");
+    }
+    else
+    {
+        Console.WriteLine(apartments +"\t\tResident");
+    }
+};
+
+
